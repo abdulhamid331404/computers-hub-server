41deb6f7b765eef98b9b9d51f611ee0eff8b87e6
abdulhamid331404 <freelancerhamid1@gmail.com>
2022-11-28 04:02:16 +0600

    connected to computer hub server

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async(req, res) =>{
+    res.send('computers hub server is running')
+});
+
+
+app.listen(port, () => console.log(`computers hub running on ${port}`))
