2f4e562eea9d1ed52c8d869d422a7c0b75c7830c
abdulhamid331404 <freelancerhamid1@gmail.com>
2022-11-28 06:50:54 +0600

    get allComputers data

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
-const port = process.env.PORT || 5000;
-
 const app = express();
+const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
@@ -14,11 +13,26 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mlgekjs.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run(){
+try{
+    const serviceCollection = client.db('computerCetagory').collection('allComputers');
+console.log(serviceCollection);
+
+ app.get('/allComputers', async (req, res) => {
+        const query = {};
+        const cursor = serviceCollection.find(query);
+        const services = await cursor.toArray();
+        console.log(services);
+        res.send(services);
+      });
+
+}
+finally{
+
+}
+}
+run().catch(console.log);
 
 
 
