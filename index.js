ddbe8998100ec8b060b3618ad890b95a0273ddd7
abdulhamid331404 <freelancerhamid1@gmail.com>
2022-11-30 22:32:07 +0600

    admine role

--- index.js
+++ index.js
@@ -63,6 +63,19 @@ async function run() {
             res.status(403).send({accessToken: ''});
         })
 
+        app.get('/users', async(req, res)=>{
+            const query = {};
+            const users = await usersCollection.find(query).toArray();
+            res.send(users);
+        })
+
+app.get('users/admin/:email', async(req, res)=>{
+  const email = req.params.email;
+    const query = { email}
+    const user = await usersCollection.findOne(query);
+    res.send({isAdmin: user?.role === 'admin'});
+})
+
         app.post('/users', async(req, res) =>{
             const user = req.body;
             const result = await usersCollection.insertOne(user);
